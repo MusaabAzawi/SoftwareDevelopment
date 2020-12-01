96bb835b0524a2285d4451b95ddd787ce7a5f010
Musaab  Azawi <chaobi92@gmail.com>
2020-12-01 11:43:39 +0100

    headers added

--- client.py
+++ client.py
@@ -1,7 +1,7 @@
 import socket
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.connect((socket.gethostname(), 1234))
+sock.connect(("127.0.0.1", 54000))
 
 while True:
 	msg = sock.recv(1024)
