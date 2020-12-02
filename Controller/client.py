f18a25cc69b1c9b0f148a970e7287b52149d45ed
MusaabAzawi <43635693+MusaabAzawi@users.noreply.github.com>
2020-12-02 13:47:35 +0100

    Delete client.py

--- Controller/client.py
+++
@@ -1,9 +0,0 @@
-import socket
-
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.connect((socket.gethostname(), 1234))
-
-while True:
-	msg = sock.recv(1024)
-	print(f"json received ")
-	print(msg.decode("utf-8"))
